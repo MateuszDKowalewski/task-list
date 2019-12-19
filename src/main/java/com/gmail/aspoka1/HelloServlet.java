6e34d9024db1622a3536c1019de27c9574371a32
aspoka <aspoka1@gmail.com>
2019-12-19 19:35:33 +0100

    Cath URL parameters

--- src/main/java/com/gmail/aspoka1/HelloServlet.java
+++ src/main/java/com/gmail/aspoka1/HelloServlet.java
@@ -9,13 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "hello", urlPatterns = {"/api/*"})
 public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARAM = "name";
+
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("Hello world!");
-        resp.getWriter().write("Hello896 world!");
+        //logger.info("Hello world!");
+        //resp.getWriter().write("Hello world!");
+
+        /*logger.info("Got request with parameters: " + req.getParameterMap());
+        String name = Optional.ofNullable(req.getParameter(NAME_PARAM)).orElse("world");
+        resp.getWriter().write("Hello " + name + "!");*/
+
+        String name = (req.getParameter(NAME_PARAM) == null) ? "world" : req.getParameter(NAME_PARAM);
+        resp.getWriter().write("Hello " + name + "!");
     }
 }
